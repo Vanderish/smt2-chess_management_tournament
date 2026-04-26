79e579089a30c8d8a00e28ab05896fd83fa6d82a
evan <mharisevan@gmail.com>
2026-04-26 10:53:30 +0700

    footer redesign

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
 <footer class="site-footer">
-    <p>&copy; <?php echo date("Y"); ?> Sistem Manajemen Turnamen Catur. Dikembangkan dengan PHP Native.</p>
+    <p>&copy; <?php echo date("Y"); ?> Sistem Manajemen Turnamen Catur.</p>
 </footer>
